dfe76c92cfb6ce48fa591d3fbc571ecf4a14019d
hiro <>
2021-07-23 15:34:30 +0900

    coomi

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,15 +1,23 @@
 import Link from "next/link";
-import styles from '../../styles/Home.module.css'
+import Head from "next/head";
+import Layout from "/components/layout";
+// import styles from '../../styles/Home.module.css'
+
 
 export default function FristPost() {
   return (
     <>
-      <h1 className={styles.title}>First Post</h1>
-      <h2 className={styles.title}>
+    <Layout>
+    <Head>
+      <title>First Post</title>
+    </Head>
+      <h1>First Post</h1>
+      <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
+      </Layout>
     </>
   );
 }
